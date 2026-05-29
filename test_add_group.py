e76cab483db030b580a42e4bbd1e3638ec7b637f
mo.murashkina <mariya.murashkina@domrf.ru>
2026-05-29 23:05:33 +0600

    Добавлен первый тест Ðдля аfirefox

---
+++ test_add_group.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+from selenium.webdriver.firefox.options import Options
+
+
+class TestAddGroup(unittest.TestCase):
+    def setUp(self):
+        options = Options()
+        options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
+
+        self.wd = webdriver.Firefox(options=options, executable_path="C:/WebDriver/geckodriver.exe")
+        self.wd.implicitly_wait(30)
+
+    def test_add_group(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("gdfbdfg")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("dfgdfg")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("dgdfg")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("Logout").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
