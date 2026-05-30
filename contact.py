06b2eeb02f2f19888cfef9bb746c9eb695f45425
mo.murashkina <mariya.murashkina@domrf.ru>
2026-05-30 18:14:20 +0600

    добавить тест на добавление контакта

---
+++ contact.py
@@ -0,0 +1,7 @@
+
+class Contact:
+    def __init__(self, first_name, last_name, email, mobile_phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.mobile_phone = mobile_phone
