8889b8fef3d4241bffcc6f5ac54df38e250328f8
mo.murashkina <mariya.murashkina@domrf.ru>
2026-05-30 15:53:25 +0600

    выделение вспомогательных методов

--- test_add_group.py
+++ test_add_group.py
@@ -2,28 +2,34 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_group(wd)
+        self.to_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd: WebDriver):
+        wd.find_element_by_link_text("Logout").click()
+
+    def to_group_page(self, wd: WebDriver):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd: WebDriver):
+        # init group creation
+        wd.find_element_by_css_selector("input[name='new']").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("gdfbdfg")
@@ -33,9 +39,20 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("dgdfg")
+        # submit group creation
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
+
+    def login(self, wd: WebDriver):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd: WebDriver):
+        wd.get("http://localhost/addressbook/group.php")
 
     def is_element_present(self, how, what):
         try:
