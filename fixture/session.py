5c69cdcee89273afec37a44fbd95c89111a97d64
mo.murashkina <mariya.murashkina@domrf.ru>
2026-06-14 17:18:13 +0600

    Добавим умный логин и логаут

--- fixture/session.py
+++ fixture/session.py
@@ -4,9 +4,25 @@ class SessionHelper:
     def __init__(self, app):
         self.app = app
 
+
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            wd.find_element_by_link_text("Logout").click()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
 
     def login(self, username, password):
         wd = self.app.wd
@@ -18,3 +34,13 @@ class SessionHelper:
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
