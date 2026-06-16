b1a623d9178f783eed684205a017ef7db89a08c8
mo.murashkina <mariya.murashkina@domrf.ru>
2026-06-16 23:03:46 +0600

    Добавить проверки количества групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="first"))
     app.group.edit_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="first"))
     app.group.edit_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
