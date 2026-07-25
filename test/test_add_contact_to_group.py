c99b960e053ddd5a1acd3d581cec0804e71cef23
mariamurashkina <maria.belyaeva.qa@gmail.com>
2026-07-25 23:02:38 +0600

    Доработка #2 по заданию #22

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -3,14 +3,13 @@ from model.contact import Contact
 import random
 
 def test_add_some_contact_to_group(app, orm):
-    contacts = orm.get_contacts_without_any_group()
-    if len(contacts) == 0:
-        app.contact.create(Contact(first_name="Contact to add to group"))
+    if len(orm.get_contacts_without_any_group()) == 0:
+        app.contact.add(Contact(first_name="Contact to add to group"))
     if len(orm.get_group_list()) == 0:
         app.group.create(Group(name="Group to test contact adding"))
     app.navigation.open_contacts_page()
-    contact = contacts[0]
+    contact_without_group = orm.get_contacts_without_any_group()[0]
     groups = orm.get_group_list()
     random_group = random.choice(groups)
-    app.contact.add_contact_to_group(contact, random_group)
-    assert str(orm.get_contacts_in_group(random_group)[0].id) == contact.id
+    app.contact.add_contact_to_group(contact_without_group, random_group)
+    assert any(str(c.id) == contact_without_group.id for c in orm.get_contacts_in_group(random_group))
